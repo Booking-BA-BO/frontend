8ebf1b65ec51d3436faef1f997e7fe0f650eb19e
bHannah <borsikaaa1@gmail.com>
2025-03-17 20:06:14 +0100

    kezdő oldal carousel és fejléc

---
+++ src/components/Galery.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+
+const Galery = () => {
+  const images = [
+    '/images/v1.jpg',
+    '/images/v2.jpg',
+    '/images/v3.jpg',
+    '/images/v4.jpg',
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 20000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className={`carousel-item ${index === currentImageIndex ? 'active' : ''}`}
+          >
+            <img
+              src={image}
+              className="d-block w-100 p-3"
+              alt={`Animation ${index}`}
+              style={{ width: '100%', maxHeight: '700px' , objectFit: 'cover' }}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Galery;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+        <div className='header-background'>
+        <h1>BookIT</h1>
+        </div>
+    </header>   
+  );
+};
+
+export default Header;
