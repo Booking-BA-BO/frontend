9e2815b1faacfc5e47642e2791310b94c2ecfc41
bHannah <borsikaaa1@gmail.com>
2025-03-18 18:40:59 +0100

    frontend hiba javítása a kezdőoldalon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Galery from './components/Galery'
-import Login from './components/Login'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from "./components/Header";
+/* import Galery from "./components/Galery"; */
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import { useContext } from "react";
+import { AppContext } from "./Context/AppContext";
+
+export default function App() {
+  const { user } = useContext(AppContext);
 
   return (
     
@@ -23,8 +29,9 @@ function App() {
   /*   return (
     <>
       <Header/>
-      <div>
-      <Card/>
+      <div className='loginsite'>
+        <Galery />
+        <Login/>
       </div>
     </>
   ) */
