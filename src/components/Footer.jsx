de4148d98a62b6f74121a3cb2a5fdecf309541a9
BeldyAdam <beldy.adam@gmail.com>
2025-03-20 10:17:27 +0100

    css módosítások

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
   return (
-    <footer className="footer bg-dark text-white py-4 w-100">
+    <footer className="footer bg-dark text-white p-4">
       <div className="container-fluid text-center">
         <p className="mb-1">© 2025 Minden jog fenntartva</p>
         <div>
-          <a href="#" className="text-white mx-2">Adatvédelem</a> | 
-          <a href="#" className="text-white mx-2">Kapcsolat</a> | 
-          <a href="#" className="text-white mx-2">Felhasználási feltételek</a>
+          <a href="" className="text-white mx-2">Adatvédelem</a> | 
+          <a href="" className="text-white mx-2">Kapcsolat</a> | 
+          <a href="" className="text-white mx-2">Felhasználási feltételek</a>
         </div>
       </div>
     </footer>
--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -1,37 +1,37 @@
 import { Button } from "react-bootstrap";
-import { Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
 import CardComponent from "../components/CardComponent";
 
 export default function Cards() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <CardComponent />
-                </div>
-                <div className="col">
-                    <CardComponent />
-                </div>
-                <div className="col">
-                    <CardComponent />
-                </div>
-                <div className="col">
-                    <CardComponent />
-                </div>
-                <div className="col">
-                    <CardComponent />
-                </div>
-                
-                <div className="col">
-        <div className="card-container">
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <CardComponent />
+        </div>
+        <div className="col">
+          <CardComponent />
+        </div>
+        <div className="col">
+          <CardComponent />
+        </div>
+        <div className="col">
+          <CardComponent />
+        </div>
+        <div className="col">
+          <CardComponent />
+        </div>
+
+        <div className="col">
+          <div className="card-container">
             <Card>
-                <Card.Body>
-                    <Button variant="primary">Hozzáad</Button>
-                </Card.Body>
+              <Card.Body>
+                <Button variant="primary">Hozzáad</Button>
+              </Card.Body>
             </Card>
+          </div>
         </div>
-                </div>
-            </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
