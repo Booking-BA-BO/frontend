e6780f835726b68e8e624f015fcd958f3001056d
bHannah <borsikaaa1@gmail.com>
2025-04-08 22:18:44 +0200

    38-as ticket profil oldal reszponzivitása elkezdve, ÁTHELYEZVE: HELP NEEDED profil oldali navigáció tervezete.

--- src/components/ProfilNavigation.jsx
+++ src/components/ProfilNavigation.jsx
@@ -1,29 +1,30 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import "../style/Profil.css";
-import { AppContext } from '../Context/AppContext';
+import { AppContext } from "../Context/AppContext";
 
 function ProfilNavigation() {
   const { user } = useContext(AppContext);
-  console.log(user)
   return (
     <nav className="profil-navigacio">
-    <ul>
-      <div><h1>{user?.name}</h1></div>
-      <li className="passive">
-        <a href="/profile/modify">Profilom szerkesztése</a>
-      </li>
-      <li className="passive">
-        <a href="/profile/events">Minden eseményem</a>
-      </li>
-      <li className="passive">
-        <a href="/stats">Statisztika</a>
-      </li>
-      <li className="passive">
-        <a href="/settings">Beállítások</a>
-      </li>
-    </ul>
-  </nav>
-  )
+      <ul>
+        <div>
+          <h1>{user?.name}</h1>
+        </div>
+        <li className="passive">
+          <a href="/profile/modify">Profilom szerkesztése</a>
+        </li>
+        <li className="passive">
+          <a href="/profile/events">Minden eseményem</a>
+        </li>
+        <li className="passive">
+          <a href="/stats">Statisztika</a>
+        </li>
+        <li className="passive">
+          <a href="/settings">Beállítások</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default ProfilNavigation
+export default ProfilNavigation;
