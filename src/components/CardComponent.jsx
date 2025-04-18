f40b8970d0096cb0534979903c4b24733f698085
BeldyAdam <beldy.adam@gmail.com>
2025-04-18 15:32:14 +0200

    cardomponent merge érthetetlen

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -7,7 +7,7 @@ const CardComponent = (props) => {
     props.elem.leiras.split(" ").slice(0, 3).join(" ") +
     (props.elem.leiras.split(" ").length > 3 ? "..." : "");
 
-    const rovidCim =
+  const rovidCim =
     props.elem.nev.split(" ").slice(0, 3).join(" ") +
     (props.elem.nev.split(" ").length > 3 ? "..." : "");
   return (
@@ -16,13 +16,13 @@ const CardComponent = (props) => {
       <div class="kartya-torzs">
         <h5 class="kartya-cim">{props.elem.nev}</h5>
         <p class="kartya-leiras">{rovidLeiras}</p>
-{/*         <button class="kartya-gomb">
-          <a href="/modifyevent">{'->'}</a>
-        </button> */}
       </div>
-      <div class="kartya-lablec">{props.elem.ar} Ft         <button class="kartya-gomb">
-          <a href="/modifyevent">{'->'}</a>
-        </button></div>
+      <div class="kartya-lablec">
+        {props.elem.ar} Ft{" "}
+        <button class="kartya-gomb">
+          <a href="/modifyevent">{"->"}</a>
+        </button>
+      </div>
     </div>
   );
 };
