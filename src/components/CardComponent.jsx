6021cc42155abb38068ba3a0f7c9ec5dfb05a8bd
BeldyAdam <beldy.adam@gmail.com>
2025-04-18 15:43:56 +0200

    68 ticket Logo a profil oldalon

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -6,10 +6,6 @@ const CardComponent = (props) => {
   const rovidLeiras =
     props.elem.leiras.split(" ").slice(0, 3).join(" ") +
     (props.elem.leiras.split(" ").length > 3 ? "..." : "");
-
-  const rovidCim =
-    props.elem.nev.split(" ").slice(0, 3).join(" ") +
-    (props.elem.nev.split(" ").length > 3 ? "..." : "");
   return (
     <div class="kartya">
       <div class="kartya-fejlec">{props.elem.hely}</div>
