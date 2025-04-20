de4383cfa01158ba511340c392ea1b76025cc7d2
BeldyAdam <beldy.adam@gmail.com>
2025-04-20 12:06:41 +0200

    88 ticket adott munkaasztal szerkesztése oldal: törzsadatok szerkesztése form css

--- src/components/EventCreationOptions.jsx
+++ src/components/EventCreationOptions.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import "../style/EventCreationOptions.css";
 
 function EventCreationOptions() {
     const { event_id } = useParams();
     return (
-            <nav>
-                <ul>
-                    <li>
+            <nav className='event-creation-options-nav'>
+                <ul className='event-creation-options-list'>
+                    <li className='event-creation-options-list-item'>
                         <a href={'/modifyevent/'+ event_id}>Törzsadatok szerkesztése</a>
                     </li>
-                    <li>
+                    <li className='event-creation-options-list-item'>
                         <a href={'/event-hosts/'+ event_id}>Esemény időpontjai</a>
                     </li>
-                    <li>
+                    <li className='event-creation-options-list-item'>
                         <a href={'/reservations/'+ event_id}>Foglalások kezelése</a>
                     </li>
                 </ul>
--- src/pages/ModifyEvent.jsx
+++ src/pages/ModifyEvent.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import EventForm from "../components/EventForm";
 import EventCreationOptions from '../components/EventCreationOptions';
+import "../style/ModifyEvent.css";
 
 function ModifyEvent() {
   return (
-    <div>
-      <div>
+    <div className='modify-event-container'>
+      <div className='event-creation-options-MAIN'>
         <EventCreationOptions />
       </div>
-      <div>
+      <div className='event-form-MAIN'>
       <EventForm />
       </div>      
     </div>
