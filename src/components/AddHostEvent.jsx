c525ec3ddeee23fd89cd74b909fddea7865ddb35
BeldyAdam <beldy.adam@gmail.com>
2025-05-10 11:32:56 +0200

    fetch authentikáció javítása

--- src/components/AddHostEvent.jsx
+++ src/components/AddHostEvent.jsx
@@ -158,6 +158,7 @@ const AddHostEvent = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
         },
         body: JSON.stringify({
           esemeny_id: event_id.event_id,
